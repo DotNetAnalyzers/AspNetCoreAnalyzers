a19c869a6264f67c3a8718588003497c3ec4d65a
JohanLarsson <johan_gavle1@hotmail.com>
2018-11-21 15:04:59 +0100

    Empty analyzer project.

---
+++ AspNetCoreAnalyzers/AnalyzerCategory.cs
@@ -0,0 +1,7 @@
+namespace AspNetCoreAnalyzers
+{
+    internal static class AnalyzerCategory
+    {
+        internal const string Correctness = "AspNetCoreAnalyzers.SystemReflection";
+    }
+}
---
+++ AspNetCoreAnalyzers/HelpLink.cs
@@ -0,0 +1,10 @@
+namespace AspNetCoreAnalyzers
+{
+    internal static class HelpLink
+    {
+        internal static string ForId(string id)
+        {
+            return $"https://github.com/DotNetAnalyzers/AspNetCoreAnalyzers/tree/master/documentation/{id}.md";
+        }
+    }
+}
