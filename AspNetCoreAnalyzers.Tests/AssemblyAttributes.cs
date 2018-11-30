18146407bd964064d0a496b523edbe9765d8d2dd
JohanLarsson <johan_gavle1@hotmail.com>
2018-11-30 12:29:56 +0100

    Fix tests.

--- AspNetCoreAnalyzers.Tests/AssemblyAttributes.cs
+++ AspNetCoreAnalyzers.Tests/AssemblyAttributes.cs
@@ -1,6 +1,7 @@
 using AspNetCoreAnalyzers.Tests;
 using Gu.Roslyn.Asserts;
 
-[assembly: MetadataReference(typeof(object), new[] { "global", "mscorlib" })]
-[assembly: MetadataReference(typeof(System.Diagnostics.Debug), new[] { "global", "System" })]
-[assembly: TransitiveMetadataReferences(typeof(ValidCodeWithAllAnalyzers))]
+[assembly: TransitiveMetadataReferences(
+    typeof(ValidCodeWithAllAnalyzers),
+    typeof(Microsoft.EntityFrameworkCore.DbContext),
+    typeof(Microsoft.AspNetCore.Mvc.Controller))]
