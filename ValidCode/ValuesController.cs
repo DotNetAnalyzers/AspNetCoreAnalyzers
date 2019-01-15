f854b88915c047a48aad433ebcf44c2f19401524
JohanLarsson <johan_gavle1@hotmail.com>
2019-01-15 15:12:04 +0100

    Valid code with multiple routes.
    #30

---
+++ ValidCode/ValuesController.cs
@@ -0,0 +1,41 @@
+namespace ValidCode
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+
+    [Route("/api/[controller]({id})")]
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return $"'{id}'";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
