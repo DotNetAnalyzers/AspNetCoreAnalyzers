f6c7b9c7540f9ea79164d252fcd3f9c89ebaad2d
JohanLarsson <johan_gavle1@hotmail.com>
2020-05-01 10:25:12 +0200

    Fix MetadataReferences

--- AspNetCoreAnalyzers.Tests/AssemblyAttributes.cs
+++ AspNetCoreAnalyzers.Tests/AssemblyAttributes.cs
@@ -1,7 +1,6 @@
-using AspNetCoreAnalyzers.Tests;
+﻿using AspNetCoreAnalyzers.Tests;
 using Gu.Roslyn.Asserts;
 
 [assembly: TransitiveMetadataReferences(
     typeof(ValidWithAllAnalyzers),
-    typeof(Microsoft.EntityFrameworkCore.DbContext),
-    typeof(Microsoft.AspNetCore.Mvc.Controller))]
+    typeof(ValidCode.Program))]
